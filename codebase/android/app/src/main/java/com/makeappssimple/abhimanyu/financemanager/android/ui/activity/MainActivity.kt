4ccc4f688b00752d7728ef532c635a7d11126cfd
Abhimanyu14 <abhimanyu.n14@gmail.com>
2021-11-21 11:41:23 +0530

    directory restructured

--- codebase/android/app/src/main/java/com/makeappssimple/abhimanyu/financemanager/android/ui/activity/MainActivity.kt
+++ codebase/android/app/src/main/java/com/makeappssimple/abhimanyu/financemanager/android/ui/activity/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.makeappssimple.abhimanyu.financemanager.android
+package com.makeappssimple.abhimanyu.financemanager.android.ui.activity
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
