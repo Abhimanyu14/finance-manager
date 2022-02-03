59ce583f4256d49f5b5185320fd600ec800648dd
Abhimanyu14 <abhimanyu.n14@gmail.com>
2022-02-03 22:54:02 +0530

    sources total

--- codebase/android/app/src/main/java/com/makeappssimple/abhimanyu/financemanager/android/ui/screens/sources/SourceListItem.kt
+++ codebase/android/app/src/main/java/com/makeappssimple/abhimanyu/financemanager/android/ui/screens/sources/SourceListItem.kt
@@ -146,7 +146,7 @@ private fun SourceListItemView(
                     color = DarkGray,
                     fontSize = 12.sp,
                     fontWeight = FontWeight.SemiBold,
-                )
+                ),
             )
         }
     }
--- codebase/android/app/src/main/java/com/makeappssimple/abhimanyu/financemanager/android/ui/screens/sources/SourcesScreenView.kt
+++ codebase/android/app/src/main/java/com/makeappssimple/abhimanyu/financemanager/android/ui/screens/sources/SourcesScreenView.kt
@@ -3,6 +3,7 @@ package com.makeappssimple.abhimanyu.financemanager.android.ui.screens.sources
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -17,10 +18,19 @@ import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.makeappssimple.abhimanyu.financemanager.android.R
 import com.makeappssimple.abhimanyu.financemanager.android.models.sortOrder
 import com.makeappssimple.abhimanyu.financemanager.android.navigation.utils.navigateToAddSourceScreen
@@ -44,6 +54,19 @@ fun SourcesScreenView(
     val sources by data.screenViewModel.sources.collectAsState(
         initial = emptyList(),
     )
+    var total by remember {
+        mutableStateOf(
+            value = "",
+        )
+    }
+
+    LaunchedEffect(
+        key1 = sources,
+    ) {
+        total = sources.sumOf {
+            it.balanceAmount.value
+        }.toString()
+    }
 
     Scaffold(
         scaffoldState = scaffoldState,
@@ -102,6 +125,23 @@ fun SourcesScreenView(
                 ),
         ) {
             LazyColumn {
+                item {
+                    Text(
+                        text = "TOTAL : $total",
+                        textAlign = TextAlign.Center,
+                        style = TextStyle(
+                            color = Primary,
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.SemiBold,
+                        ),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(
+                                horizontal = 16.dp,
+                                vertical = 8.dp,
+                            ),
+                    )
+                }
                 itemsIndexed(
                     items = sources
                         .sortedWith(
