c549af72f07b47032ce8f757c58aaed3d4c8d71e
Abhimanyu14 <abhimanyu.n14@gmail.com>
2022-03-26 00:13:04 +0530

    edit balance amount bottom sheet

---
+++ codebase/android/app/src/main/java/com/makeappssimple/abhimanyu/financemanager/android/ui/screens/sources/SourcesEditBalanceAmountBottomSheet.kt
@@ -0,0 +1,127 @@
+package com.makeappssimple.abhimanyu.financemanager.android.ui.screens.sources
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+data class SourcesEditBalanceAmountBottomSheetData(
+    val balanceAmount: Int,
+    val updateBalanceAmount: (updatedBalanceAmount: Int) -> Unit,
+)
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun SourcesEditBalanceAmountBottomSheet(
+    data: SourcesEditBalanceAmountBottomSheetData,
+) {
+    val focusManager = LocalFocusManager.current
+    val focusRequester = remember {
+        FocusRequester()
+    }
+    var updatedBalanceAmount by remember {
+        mutableStateOf(
+            value = TextFieldValue(
+                text = data.balanceAmount.toString(),
+                selection = TextRange(
+                    start = data.balanceAmount.toString().length,
+                    end = data.balanceAmount.toString().length,
+                ),
+            ),
+        )
+    }
+
+    LaunchedEffect(
+        key1 = Unit,
+    ) {
+        focusRequester.requestFocus()
+    }
+
+    Column(
+        modifier = Modifier
+            .padding(
+                all = 16.dp,
+            )
+            .fillMaxWidth(),
+    ) {
+        Text(
+            text = "Edit Balance Amount",
+            style = TextStyle(
+                color = Color.DarkGray,
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Bold,
+            ),
+            modifier = Modifier
+                .fillMaxWidth(),
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            BasicTextField(
+                value = updatedBalanceAmount,
+                onValueChange = { value: TextFieldValue ->
+                    updatedBalanceAmount = value
+                },
+                keyboardActions = KeyboardActions(
+                    onDone = {
+                        focusManager.clearFocus()
+                    },
+                ),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.NumberPassword,
+                    imeAction = ImeAction.Next,
+                ),
+                modifier = Modifier
+                    .weight(
+                        weight = 1F,
+                    )
+                    .focusRequester(
+                        focusRequester = focusRequester,
+                    ),
+                singleLine = true,
+            )
+            TextButton(
+                onClick = {
+                    val balanceAmount = if (updatedBalanceAmount.text.isNotBlank()) {
+                        updatedBalanceAmount.text.toInt()
+                    } else {
+                        0
+                    }
+                    data.updateBalanceAmount(
+                        balanceAmount
+                    )
+                },
+            ) {
+                Text(text = "Save")
+            }
+        }
+    }
+}
--- codebase/android/app/src/main/java/com/makeappssimple/abhimanyu/financemanager/android/ui/screens/sources/SourcesScreenView.kt
+++ codebase/android/app/src/main/java/com/makeappssimple/abhimanyu/financemanager/android/ui/screens/sources/SourcesScreenView.kt
@@ -3,105 +3,187 @@ package com.makeappssimple.abhimanyu.financemanager.android.ui.screens.sources
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.FabPosition
+import androidx.compose.material.ModalBottomSheetLayout
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.makeappssimple.abhimanyu.financemanager.android.R
 import com.makeappssimple.abhimanyu.financemanager.android.models.Amount
+import com.makeappssimple.abhimanyu.financemanager.android.models.Source
 import com.makeappssimple.abhimanyu.financemanager.android.models.sortOrder
 import com.makeappssimple.abhimanyu.financemanager.android.navigation.utils.navigateToAddSourceScreen
+import com.makeappssimple.abhimanyu.financemanager.android.ui.common.EmptySpace
 import com.makeappssimple.abhimanyu.financemanager.android.ui.common.MyFloatingActionButton
 import com.makeappssimple.abhimanyu.financemanager.android.ui.common.MyTopAppBar
 import com.makeappssimple.abhimanyu.financemanager.android.ui.common.ScaffoldContentWrapper
 import com.makeappssimple.abhimanyu.financemanager.android.ui.common.TotalBalanceCard
+import com.makeappssimple.abhimanyu.financemanager.android.ui.common.toggleModalBottomSheetState
+
+enum class SourcesBottomSheetType {
+    NONE,
+    EDIT_BALANCE_AMOUNT,
+}
 
 data class SourcesScreenViewData(
     val screenViewModel: SourcesViewModel,
 )
 
+@OptIn(ExperimentalComposeUiApi::class)
 @ExperimentalMaterialApi
 @Composable
 fun SourcesScreenView(
     data: SourcesScreenViewData,
     state: SourcesScreenViewState,
 ) {
+    val keyboardController = LocalSoftwareKeyboardController.current
     val sources by data.screenViewModel.sources.collectAsState(
         initial = emptyList(),
     )
     val totalBalanceAmount by data.screenViewModel.sourcesTotalBalanceAmountValue.collectAsState(
         initial = 0L,
     )
+    var clickedSource: Source? by remember {
+        mutableStateOf(
+            value = null,
+        )
+    }
+    var sourcesBottomSheetType by remember {
+        mutableStateOf(
+            value = SourcesBottomSheetType.NONE,
+        )
+    }
 
-    Scaffold(
-        scaffoldState = state.scaffoldState,
-        topBar = {
-            MyTopAppBar(
-                navigationManager = data.screenViewModel.navigationManager,
-                titleText = stringResource(
-                    id = R.string.screen_sources_appbar_title,
-                ),
-                isNavigationIconVisible = true,
-            )
-        },
-        floatingActionButton = {
-            MyFloatingActionButton(
-                contentDescription = stringResource(
-                    id = R.string.screen_sources_floating_action_button_content_description,
-                ),
-                onClick = {
-                    navigateToAddSourceScreen(
-                        navigationManager = data.screenViewModel.navigationManager,
-                    )
-                },
-            )
-        },
-        floatingActionButtonPosition = FabPosition.End,
-        isFloatingActionButtonDocked = true,
-        modifier = Modifier
-            .fillMaxSize(),
-    ) { innerPadding ->
-        ScaffoldContentWrapper(
-            innerPadding = innerPadding,
-            onClick = {
-                state.focusManager.clearFocus()
-            },
-        ) {
-            LazyColumn {
-                item {
-                    TotalBalanceCard(
-                        total = Amount(
-                            value = totalBalanceAmount,
-                        ).toString(),
+    ModalBottomSheetLayout(
+        sheetState = state.modalBottomSheetState,
+        sheetShape = RoundedCornerShape(
+            topStart = 16.dp,
+            topEnd = 16.dp,
+        ),
+        sheetContent = {
+            when (sourcesBottomSheetType) {
+                SourcesBottomSheetType.NONE -> {
+                    EmptySpace()
+                }
+                SourcesBottomSheetType.EDIT_BALANCE_AMOUNT -> {
+                    SourcesEditBalanceAmountBottomSheet(
+                        data = SourcesEditBalanceAmountBottomSheetData(
+                            balanceAmount = clickedSource?.balanceAmount?.value?.toInt() ?: 0,
+                            updateBalanceAmount = { updatedBalance ->
+                                toggleModalBottomSheetState(
+                                    coroutineScope = state.coroutineScope,
+                                    modalBottomSheetState = state.modalBottomSheetState,
+                                ) {
+                                    clickedSource?.let {
+                                        val updatedSource = it.copy(
+                                            balanceAmount = it.balanceAmount.copy(
+                                                value = updatedBalance.toLong(),
+                                            ),
+                                        )
+                                        data.screenViewModel.updateSource(
+                                            source = updatedSource,
+                                        )
+                                    }
+
+                                    // TODO-Abhi: Update balance amount
+                                    sourcesBottomSheetType = SourcesBottomSheetType.NONE
+                                    keyboardController?.hide()
+                                }
+                            },
+                        ),
                     )
                 }
-                itemsIndexed(
-                    items = sources
-                        .sortedWith(
-                            comparator = compareBy {
-                                it.type.sortOrder
-                            }
-                        ),
-                    key = { _, listItem ->
-                        listItem.hashCode()
+            }
+        },
+    ) {
+        Scaffold(
+            scaffoldState = state.scaffoldState,
+            topBar = {
+                MyTopAppBar(
+                    navigationManager = data.screenViewModel.navigationManager,
+                    titleText = stringResource(
+                        id = R.string.screen_sources_appbar_title,
+                    ),
+                    isNavigationIconVisible = true,
+                )
+            },
+            floatingActionButton = {
+                MyFloatingActionButton(
+                    contentDescription = stringResource(
+                        id = R.string.screen_sources_floating_action_button_content_description,
+                    ),
+                    onClick = {
+                        navigateToAddSourceScreen(
+                            navigationManager = data.screenViewModel.navigationManager,
+                        )
                     },
-                ) { _, listItem ->
-                    SourceListItem(
-                        source = listItem,
-                        swipeToDeleteEnabled = !listItem.name.contains(
-                            other = "Cash",
-                            ignoreCase = false,
-                        ),
-                        deleteSource = {
-                            data.screenViewModel.deleteSource(
-                                id = listItem.id,
-                            )
+                )
+            },
+            floatingActionButtonPosition = FabPosition.End,
+            isFloatingActionButtonDocked = true,
+            modifier = Modifier
+                .fillMaxSize(),
+        ) { innerPadding ->
+            ScaffoldContentWrapper(
+                innerPadding = innerPadding,
+                onClick = {
+                    state.focusManager.clearFocus()
+                },
+            ) {
+                LazyColumn {
+                    item {
+                        TotalBalanceCard(
+                            total = Amount(
+                                value = totalBalanceAmount,
+                            ).toString(),
+                        )
+                    }
+                    itemsIndexed(
+                        items = sources
+                            .sortedWith(
+                                comparator = compareBy {
+                                    it.type.sortOrder
+                                }
+                            ),
+                        key = { _, listItem ->
+                            listItem.hashCode()
                         },
-                    )
+                    ) { _, listItem ->
+                        SourceListItem(
+                            source = listItem,
+                            swipeToDeleteEnabled = !listItem.name.contains(
+                                other = "Cash",
+                                ignoreCase = false,
+                            ),
+                            deleteSource = {
+                                data.screenViewModel.deleteSource(
+                                    id = listItem.id,
+                                )
+                            },
+                            onClick = {
+                                /*
+                                TODO-Abhi: Source edit balance amount bottom sheet on click
+                                clickedSource = listItem
+                                sourcesBottomSheetType = SourcesBottomSheetType.EDIT_BALANCE_AMOUNT
+                                toggleModalBottomSheetState(
+                                    coroutineScope = state.coroutineScope,
+                                    modalBottomSheetState = state.modalBottomSheetState,
+                                ) {}
+                                */
+                            },
+                        )
+                    }
                 }
             }
         }
