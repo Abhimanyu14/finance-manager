cbe7734755f7c30a6d72619ff143aa30ef2fed15
Abhimanyu14 <abhimanyu.n14@gmail.com>
2022-07-27 07:15:38 +0530

    [TECH] Created catalog app

---
+++ codebase/android/app-ui-catalog/src/main/java/com/makeappssimple/abhimanyu/financemanager/android/app/ui/catalog/CatalogActivity.kt
@@ -0,0 +1,34 @@
+package com.makeappssimple.abhimanyu.financemanager.android.app.ui.catalog
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.makeappssimple.abhimanyu.financemanager.android.app.ui.catalog.ui.theme.FinanceManagerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FinanceManagerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
