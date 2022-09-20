de6a4ee5f55501223e9b75f4bf40189eb337f625
Abhimanyu14 <abhimanyu.n14@gmail.com>
2022-09-20 22:16:14 +0530

    [TECH] Status bar icons

--- codebase/android/app/src/main/java/com/makeappssimple/abhimanyu/financemanager/android/MainActivity.kt
+++ codebase/android/app/src/main/java/com/makeappssimple/abhimanyu/financemanager/android/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.makeappssimple.abhimanyu.financemanager.android
 
 import android.app.Activity
-import android.os.Build
 import android.os.Bundle
-import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.view.WindowInsetsControllerCompat
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -23,8 +22,13 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+/**
+ * Source: https://stackoverflow.com/a/71036723/9636037
+ */
 fun Activity.setDarkStatusBarIcons() {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+    window?.let { window ->
+        WindowInsetsControllerCompat(window, window.decorView)
+    }?.apply {
+        isAppearanceLightStatusBars = true
     }
 }
