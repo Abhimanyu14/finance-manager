7478d23cec4e5611aed372e59d66410d7d2c0654
Abhimanyu14 <abhimanyu.n14@gmail.com>
2023-03-16 22:48:33 +0530

    [TECH] UT

---
+++ codebase/android/core-database/src/test/java/com/makeappssimple/abhimanyu/financemanager/android/core/database/source/usecase/UpdateSourcesBalanceAmountUseCaseTest.kt
@@ -0,0 +1,41 @@
+package com.makeappssimple.abhimanyu.financemanager.android.core.database.source.usecase
+
+import com.makeappssimple.abhimanyu.financemanager.android.core.database.source.repository.SourceRepository
+import com.makeappssimple.abhimanyu.financemanager.android.core.datastore.MyDataStore
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+
+internal class UpdateSourcesBalanceAmountUseCaseTest {
+    private val dataStore: MyDataStore = mock()
+    private val sourceRepository: SourceRepository = mock()
+    private lateinit var updateSourcesBalanceAmountUseCase: UpdateSourcesBalanceAmountUseCase
+
+    @Before
+    fun setUp() {
+        updateSourcesBalanceAmountUseCase = UpdateSourcesBalanceAmountUseCaseImpl(
+            dataStore = dataStore,
+            sourceRepository = sourceRepository,
+        )
+    }
+
+    @Test
+    fun invoke_defaultTest() = runTest {
+        val testData = listOf(
+            Pair(0, 1L),
+            Pair(1, 2L),
+            Pair(2, 3L),
+        )
+        updateSourcesBalanceAmountUseCase(
+            testData,
+        )
+
+        verify(
+            mock = sourceRepository,
+        ).updateSourceBalanceAmount(
+            testData,
+        )
+    }
+}
