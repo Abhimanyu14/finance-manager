ab69524dd6cf5967a21463f5a6ae7f234480a42b
Abhimanyu14 <abhimanyu.n14@gmail.com>
2023-11-19 11:43:42 +0530

    [TECH] code cleanup

--- codebase/android/core/logger/src/main/java/com/makeappssimple/abhimanyu/financemanager/android/core/logger/LocalMyLogger.kt
+++ codebase/android/core/logger/src/main/java/com/makeappssimple/abhimanyu/financemanager/android/core/logger/LocalMyLogger.kt
@@ -3,7 +3,8 @@ package com.makeappssimple.abhimanyu.financemanager.android.core.logger
 import androidx.compose.runtime.staticCompositionLocalOf
 
 val LocalMyLogger = staticCompositionLocalOf<MyLogger> {
-    // Provide a default AnalyticsHelper which does nothing. This is so that tests and previews
-    // do not have to provide one. For real app builds provide a different implementation.
+    // Provide a default MyLogger which does nothing.
+    // This is so that tests and previews do not have to provide one.
+    // For real app builds provide a different implementation.
     NoOpMyLoggerImpl()
 }
