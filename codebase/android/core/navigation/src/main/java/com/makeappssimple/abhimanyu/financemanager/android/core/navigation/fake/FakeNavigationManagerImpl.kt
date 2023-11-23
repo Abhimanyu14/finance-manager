c136c201b449fdbb2ba05a09ea749b2f637387ad
Abhimanyu14 <abhimanyu.n14@gmail.com>
2023-11-23 14:26:54 +0530

    [TECH] FakeNavigationManagerImpl

--- codebase/android/core/navigation/src/main/java/com/makeappssimple/abhimanyu/financemanager/android/core/navigation/fake/FakeNavigationManagerImpl.kt
+++ codebase/android/core/navigation/src/main/java/com/makeappssimple/abhimanyu/financemanager/android/core/navigation/fake/FakeNavigationManagerImpl.kt
@@ -5,7 +5,6 @@ import com.makeappssimple.abhimanyu.financemanager.android.core.navigation.Navig
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
-import kotlinx.coroutines.launch
 
 class FakeNavigationManagerImpl(
     private val coroutineScope: CoroutineScope,
@@ -13,11 +12,83 @@ class FakeNavigationManagerImpl(
     private val _command: MutableSharedFlow<NavigationCommand> = MutableSharedFlow()
     override val command: SharedFlow<NavigationCommand> = _command
 
-    override fun navigate(
-        navigationCommand: NavigationCommand,
-    ) {
-        coroutineScope.launch {
-            _command.emit(navigationCommand)
-        }
+//    override fun navigate(
+//        navigationCommand: NavigationCommand,
+//    ) {
+//        coroutineScope.launch {
+//            _command.emit(navigationCommand)
+//        }
+//    }
+
+    override fun navigateToAccountsScreen() {
+        TODO("Not yet implemented")
+    }
+
+    override fun navigateToAddAccountScreen() {
+        TODO("Not yet implemented")
+    }
+
+    override fun navigateToAddCategoryScreen(transactionType: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun navigateToAddTransactionScreen(transactionId: Int?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun navigateToAddTransactionForScreen() {
+        TODO("Not yet implemented")
+    }
+
+    override fun navigateToAnalysisScreen() {
+        TODO("Not yet implemented")
+    }
+
+    override fun navigateToCategoriesScreen() {
+        TODO("Not yet implemented")
+    }
+
+    override fun navigateToEditAccountScreen(accountId: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun navigateToEditCategoryScreen(categoryId: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun navigateToEditTransactionScreen(transactionId: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun navigateToEditTransactionForScreen(transactionForId: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun navigateToHomeScreen() {
+        TODO("Not yet implemented")
+    }
+
+    override fun navigateToOpenSourceLicensesScreen() {
+        TODO("Not yet implemented")
+    }
+
+    override fun navigateToSettingsScreen() {
+        TODO("Not yet implemented")
+    }
+
+    override fun navigateToTransactionForValuesScreen() {
+        TODO("Not yet implemented")
+    }
+
+    override fun navigateToTransactionsScreen() {
+        TODO("Not yet implemented")
+    }
+
+    override fun navigateToViewTransactionScreen(transactionId: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun navigateUp() {
+        TODO("Not yet implemented")
     }
 }
