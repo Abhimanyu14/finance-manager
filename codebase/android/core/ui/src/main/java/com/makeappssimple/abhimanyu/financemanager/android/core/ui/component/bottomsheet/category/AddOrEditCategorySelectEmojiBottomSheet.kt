61c104fa220d25db0648bfad00a61f145a27eff1
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-03-24 15:18:29 +0530

    [TECH] compose emoji picker version upgrade

--- codebase/android/core/ui/src/main/java/com/makeappssimple/abhimanyu/financemanager/android/core/ui/component/bottomsheet/category/AddOrEditCategorySelectEmojiBottomSheet.kt
+++ codebase/android/core/ui/src/main/java/com/makeappssimple/abhimanyu/financemanager/android/core/ui/component/bottomsheet/category/AddOrEditCategorySelectEmojiBottomSheet.kt
@@ -4,6 +4,7 @@ import android.widget.Toast
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 import com.makeappssimple.abhimanyu.composeemojipicker.ComposeEmojiPickerBottomSheetUI
+import com.makeappssimple.abhimanyu.composeemojipicker.Emoji
 import com.makeappssimple.abhimanyu.financemanager.android.core.common.extensions.capitalizeWords
 
 @Composable
@@ -16,11 +17,11 @@ fun AddOrEditCategorySelectEmojiBottomSheet(
     val context = LocalContext.current
     ComposeEmojiPickerBottomSheetUI(
         searchText = searchText,
-        onEmojiClick = { emoji ->
+        onEmojiClick = { emoji: Emoji ->
             resetBottomSheetType()
             updateEmoji(emoji.character)
         },
-        onEmojiLongClick = { emoji ->
+        onEmojiLongClick = { emoji: Emoji ->
             Toast.makeText(
                 context,
                 emoji.unicodeName.capitalizeWords(),
