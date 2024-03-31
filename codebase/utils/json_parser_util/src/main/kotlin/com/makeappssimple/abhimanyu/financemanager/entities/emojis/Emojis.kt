0aba42155c0357a86469b79f45e0a2faa139eafc
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-03-31 21:01:42 +0530

    [TECH] removed utils - json parser code

--- codebase/utils/json_parser_util/src/main/kotlin/com/makeappssimple/abhimanyu/financemanager/entities/emojis/Emojis.kt
+++
@@ -1,8 +0,0 @@
-package com.makeappssimple.abhimanyu.financemanager.entities.emojis
-
-import com.makeappssimple.abhimanyu.financemanager.entities.emoji.Emoji
-
-interface Emojis {
-    val versionNumber: Int
-    val emojisData: List<Emoji>
-}
