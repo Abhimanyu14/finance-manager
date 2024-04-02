f8699bb8cde69b577064b9439f60dc10a3e5ea04
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-04-02 22:08:26 +0530

    [TECH] code clean up

--- codebase/android/core/configkitimpl/src/main/java/com/makeappssimple/abhimanyu/financemanager/android/core/configkitimpl/ConfigKitImpl.kt
+++ codebase/android/core/configkitimpl/src/main/java/com/makeappssimple/abhimanyu/financemanager/android/core/configkitimpl/ConfigKitImpl.kt
@@ -3,10 +3,8 @@ package com.makeappssimple.abhimanyu.financemanager.android.core.configkitimpl
 import android.content.Context
 import com.google.firebase.Firebase
 import com.google.firebase.remoteconfig.remoteConfig
-import com.makeappssimple.abhimanyu.financemanager.android.core.common.datetime.DateTimeUtil
 import com.makeappssimple.abhimanyu.financemanager.android.core.configkit.ConfigKit
 import com.makeappssimple.abhimanyu.financemanager.android.core.data.repository.preferences.MyPreferencesRepository
-import com.makeappssimple.abhimanyu.financemanager.android.core.logger.MyLogger
 import kotlinx.coroutines.CoroutineDispatcher
 
 class ConfigKitImpl(
@@ -15,7 +13,6 @@ class ConfigKitImpl(
     private val myPreferencesRepository: MyPreferencesRepository,
 ) : ConfigKit {
     override fun isEnabled(): Boolean {
-        //
         Firebase.remoteConfig
         return true
     }
--- codebase/android/core/designsystem/src/main/java/com/makeappssimple/abhimanyu/financemanager/android/core/designsystem/component/button/MyFloatingActionButton.kt
+++ codebase/android/core/designsystem/src/main/java/com/makeappssimple/abhimanyu/financemanager/android/core/designsystem/component/button/MyFloatingActionButton.kt
@@ -7,9 +7,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.semantics.onClick
-import androidx.compose.ui.semantics.semantics
-import com.makeappssimple.abhimanyu.financemanager.android.core.designsystem.typealiases.ComposableContent
 
 @Composable
 fun MyFloatingActionButton(
@@ -30,24 +27,4 @@ fun MyFloatingActionButton(
             tint = MaterialTheme.colorScheme.onPrimary,
         )
     }
-}
-
-@Composable
-fun MyFloatingActionButton(
-    modifier: Modifier = Modifier,
-    onClickLabel: String,
-    onClick: () -> Unit,
-    content: ComposableContent,
-) {
-    FloatingActionButton(
-        onClick = onClick,
-        content = content,
-        modifier = modifier
-            .semantics {
-                onClick(
-                    label = onClickLabel,
-                    action = null,
-                )
-            },
-    )
 }
