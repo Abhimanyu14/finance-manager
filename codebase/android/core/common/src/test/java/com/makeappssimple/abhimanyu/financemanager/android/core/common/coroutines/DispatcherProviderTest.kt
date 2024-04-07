b65079cb3ff440b23fc66511cbf4200eefea678b
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-04-07 15:51:21 +0530

    [TECH] DispatcherProviderTest

--- codebase/android/core/common/src/test/java/com/makeappssimple/abhimanyu/financemanager/android/core/common/coroutines/DispatcherProviderTest.kt
+++ codebase/android/core/common/src/test/java/com/makeappssimple/abhimanyu/financemanager/android/core/common/coroutines/DispatcherProviderTest.kt
@@ -1,41 +1,48 @@
 package com.makeappssimple.abhimanyu.financemanager.android.core.common.coroutines
 
+import com.makeappssimple.abhimanyu.financemanager.android.core.common.coroutines.di.DefaultDispatcher
+import com.makeappssimple.abhimanyu.financemanager.android.core.common.coroutines.di.IoDispatcher
+import com.makeappssimple.abhimanyu.financemanager.android.core.common.coroutines.di.MainDispatcher
+import com.makeappssimple.abhimanyu.financemanager.android.core.common.coroutines.di.MainImmediateDispatcher
+import com.makeappssimple.abhimanyu.financemanager.android.core.common.coroutines.di.UnconfinedDispatcher
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
+import javax.inject.Inject
 
 class DispatcherProviderTest {
-//    @Inject
-//    @DefaultDispatcher
-//    private lateinit var defaultDispatcher: CoroutineDispatcher
-//
-//    @Inject
-//    @IoDispatcher
-//    private lateinit var ioDispatcher: CoroutineDispatcher
-//
-//    @Inject
-//    @MainDispatcher
-//    private lateinit var mainDispatcher: CoroutineDispatcher
-//
-//    @Inject
-//    @MainImmediateDispatcher
-//    private lateinit var mainImmediateDispatcher: CoroutineDispatcher
-//
-//    @Inject
-//    @UnconfinedDispatcher
-//    private lateinit var unconfinedDispatcher: CoroutineDispatcher
+    @Inject
+    @DefaultDispatcher
+    private lateinit var defaultDispatcher: CoroutineDispatcher
+
+    @Inject
+    @IoDispatcher
+    private lateinit var ioDispatcher: CoroutineDispatcher
+
+    @Inject
+    @MainDispatcher
+    private lateinit var mainDispatcher: CoroutineDispatcher
+
+    @Inject
+    @MainImmediateDispatcher
+    private lateinit var mainImmediateDispatcher: CoroutineDispatcher
+
+    @Inject
+    @UnconfinedDispatcher
+    private lateinit var unconfinedDispatcher: CoroutineDispatcher
 
     private lateinit var dispatcherProvider: DispatcherProvider
 
     @Before
     fun setUp() {
         dispatcherProvider = DispatcherProviderImpl(
-//            defaultDispatcher = defaultDispatcher,
-//            ioDispatcher = ioDispatcher,
-//            mainDispatcher = mainDispatcher,
-//            mainImmediateDispatcher = mainImmediateDispatcher,
-//            unconfinedDispatcher = unconfinedDispatcher,
+            defaultDispatcher = defaultDispatcher,
+            ioDispatcher = ioDispatcher,
+            mainDispatcher = mainDispatcher,
+            mainImmediateDispatcher = mainImmediateDispatcher,
+            unconfinedDispatcher = unconfinedDispatcher,
         )
     }
 
