d58715308ef260859602aee59d82e55f62088f3d
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-04-27 17:46:03 +0530

    [FEAT] TransactionForListItem shape changes

--- codebase/android/core/ui/src/main/java/com/makeappssimple/abhimanyu/financemanager/android/core/ui/component/listitem/transactionfor/TransactionForListItem.kt
+++ codebase/android/core/ui/src/main/java/com/makeappssimple/abhimanyu/financemanager/android/core/ui/component/listitem/transactionfor/TransactionForListItem.kt
@@ -6,7 +6,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import com.makeappssimple.abhimanyu.financemanager.android.core.designsystem.component.MyText
 import com.makeappssimple.abhimanyu.financemanager.android.core.designsystem.extensions.conditionalClickable
@@ -36,12 +35,6 @@ public fun TransactionForListItem(
     MyText(
         modifier = modifier
             .fillMaxWidth()
-            .padding(
-                horizontal = 8.dp,
-            )
-            .clip(
-                shape = MaterialTheme.shapes.large,
-            )
             .conditionalClickable(
                 onClick = events.onClick,
             )
