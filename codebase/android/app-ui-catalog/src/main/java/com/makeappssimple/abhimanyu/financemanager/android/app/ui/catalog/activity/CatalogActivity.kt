0c428fffa4f95d8a323a4f1d7a4f0e431fbebc65
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-05-03 17:39:22 +0530

    [TECH] dark icons in status bar for catalog app

--- codebase/android/app-ui-catalog/src/main/java/com/makeappssimple/abhimanyu/financemanager/android/app/ui/catalog/activity/CatalogActivity.kt
+++ codebase/android/app-ui-catalog/src/main/java/com/makeappssimple/abhimanyu/financemanager/android/app/ui/catalog/activity/CatalogActivity.kt
@@ -1,24 +1,20 @@
 package com.makeappssimple.abhimanyu.financemanager.android.app.ui.catalog.activity
 
-import android.app.Activity
 import android.os.Bundle
-import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 
 public class CatalogActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setDarkStatusBarIcons()
+        // Turn off the decor fitting system windows, which allows us to handle insets,
+        // including IME animations
+        enableEdgeToEdge()
 
         setContent {
             CatalogApp()
         }
     }
-}
-
-public fun Activity.setDarkStatusBarIcons() {
-    window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
 }
--- codebase/android/app/src/main/java/com/makeappssimple/abhimanyu/financemanager/android/MainActivity.kt
+++ codebase/android/app/src/main/java/com/makeappssimple/abhimanyu/financemanager/android/MainActivity.kt
@@ -12,7 +12,6 @@ import javax.inject.Inject
 
 @AndroidEntryPoint
 public class MainActivity : ComponentActivity() {
-
     @Inject
     public lateinit var myLogger: MyLogger
 
