3846a34a2f782014f1fd12a0302a8895a3c21466
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-05-26 23:58:59 +0530

    [TECH] ScreenUIData clean up

--- codebase/android/feature/settings/src/main/java/com/makeappssimple/abhimanyu/financemanager/android/feature/settings/open_source_licenses/screen/OpenSourceLicensesScreenUIData.kt
+++
@@ -1,9 +0,0 @@
-package com.makeappssimple.abhimanyu.financemanager.android.feature.settings.open_source_licenses.screen
-
-import androidx.compose.runtime.Immutable
-import com.makeappssimple.abhimanyu.financemanager.android.core.ui.base.ScreenUIData
-
-@Immutable
-public data class OpenSourceLicensesScreenUIData(
-    val isLoading: Boolean = false,
-) : ScreenUIData
