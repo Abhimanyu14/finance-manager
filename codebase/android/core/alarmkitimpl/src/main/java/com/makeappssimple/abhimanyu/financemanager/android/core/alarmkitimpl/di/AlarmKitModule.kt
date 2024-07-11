e229b854247095a344c992407e9ed8f43796e782
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-07-11 20:15:13 +0530

    [TECH] AlarmKit changes: Removed DispatcherProvider dependency

--- codebase/android/core/alarmkitimpl/src/main/java/com/makeappssimple/abhimanyu/financemanager/android/core/alarmkitimpl/di/AlarmKitModule.kt
+++ codebase/android/core/alarmkitimpl/src/main/java/com/makeappssimple/abhimanyu/financemanager/android/core/alarmkitimpl/di/AlarmKitModule.kt
@@ -3,7 +3,6 @@ package com.makeappssimple.abhimanyu.financemanager.android.core.alarmkitimpl.di
 import android.content.Context
 import com.makeappssimple.abhimanyu.financemanager.android.core.alarmkit.AlarmKit
 import com.makeappssimple.abhimanyu.financemanager.android.core.alarmkitimpl.AlarmKitImpl
-import com.makeappssimple.abhimanyu.financemanager.android.core.common.coroutines.DispatcherProvider
 import com.makeappssimple.abhimanyu.financemanager.android.core.common.datetime.DateTimeUtil
 import com.makeappssimple.abhimanyu.financemanager.android.core.data.repository.preferences.MyPreferencesRepository
 import com.makeappssimple.abhimanyu.financemanager.android.core.logger.MyLogger
@@ -19,7 +18,6 @@ public class AlarmKitModule {
     @Provides
     public fun providesAlarmKit(
         @ApplicationContext context: Context,
-        dispatcherProvider: DispatcherProvider,
         dateTimeUtil: DateTimeUtil,
         myLogger: MyLogger,
         myPreferencesRepository: MyPreferencesRepository,
