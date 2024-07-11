6c3f252309456258ff998c3a784eb0994182491b
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-07-11 21:31:58 +0530

    [TECH] Detekt fixes - ModifierWithoutDefault

--- codebase/android/core/ui/src/main/java/com/makeappssimple/abhimanyu/financemanager/android/core/ui/component/actionbutton/ActionButton.kt
+++ codebase/android/core/ui/src/main/java/com/makeappssimple/abhimanyu/financemanager/android/core/ui/component/actionbutton/ActionButton.kt
@@ -43,7 +43,7 @@ public fun ActionButton(
 
 @Composable
 private fun ActionButtonUI(
-    modifier: Modifier,
+    modifier: Modifier = Modifier,
     data: ActionButtonData,
     handleEvent: (event: ActionButtonEvent) -> Unit,
 ) {
